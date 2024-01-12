627a573a99f4e5c22b688231fb40d3421a5f9881
SebastianAdamoszek <sebaadamoszek@vp.pl>
2024-01-12 18:49:26 +0100

    test

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import  App from 'components/App';
 import './index.css';
-//dla Redux
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
 
@@ -11,4 +10,3 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <App />
   </Provider>
 );
-
